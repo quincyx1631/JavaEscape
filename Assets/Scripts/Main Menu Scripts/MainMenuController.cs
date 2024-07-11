2cf0381e5a5d9e64c1bbe5347f138cc0abfa04e7
Shin3021 <shinruuu21@gmail.com>
2024-07-11 17:48:39 +0800

    Main Manu
    Main Menu
    GUMAWA AKO NG MAIN MENU

---
+++ Assets/Scripts/Main Menu Scripts/MainMenuController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Levels to Load")]
+    public string _newGameLevel;
+    private string levelToLoad;
+    [SerializeField] private GameObject noSavedGameDialog = null;
+
+    public void newGameDialogYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+
+    public void LoadGameDialogYes()
+    {
+        if (PlayerPrefs.HasKey("SavedLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSavedGameDialog.SetActive(true);
+        }
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
