07cd0115594c611d4f4d9271b7fdb680f3fdca99
javenzz <manalomarkjaven@gmail.com>
2024-07-17 17:37:46 +0800

    NATAPOS NA YUNG QUIZ
    KAILANGAN:
    LEVEL PANEL
    QUESTION AND ANSWERS
    LONG QUIZ
    BACK
    QUIZ SA MAIN MENU

--- Assets/Scripts/Main Menu Scripts/QuestionAndAnswer.cs
+++ Assets/Scripts/Main Menu Scripts/QuestionAndAnswer.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
+using UnityEngine.UI;
 
-public class QuestionAndAnswer : MonoBehaviour
+[System.Serializable]
+public class QuestionAndAnswer
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string Question;
+    public string[] Answers;
+    public int CorrectAnswer;
+
 }
