d296c7cd28c2ad76cf5c60d8807dcb6b0f911e80
quincyx1631 <quincyx1631@gmail.com>
2024-11-16 22:06:05 +0800

    Revert "End Credit Fix"
    This reverts commit 56b543cf4550a685d4d2bc10124289a9a643381b.

--- Assets/VideoEndRedirect.cs
+++ Assets/VideoEndRedirect.cs
@@ -15,7 +15,6 @@ public class VideoEndRedirect : MonoBehaviour
     private void OnVideoEnd(VideoPlayer vp)
     {
         // Load the main menu after the video finishes
-        /*SceneManager.LoadScene("Main Menu Final");*/ // Replace "MainMenu" with your actual main menu scene name
-        LoadingScreenManager.Instance.StartLoading();
+        SceneManager.LoadScene("Main Menu Final"); // Replace "MainMenu" with your actual main menu scene name
     }
 }
