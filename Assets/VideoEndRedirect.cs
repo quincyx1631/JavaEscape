56b543cf4550a685d4d2bc10124289a9a643381b
quincyx1631 <quincyx1631@gmail.com>
2024-11-16 21:50:31 +0800

    End Credit Fix

--- Assets/VideoEndRedirect.cs
+++ Assets/VideoEndRedirect.cs
@@ -15,6 +15,7 @@ public class VideoEndRedirect : MonoBehaviour
     private void OnVideoEnd(VideoPlayer vp)
     {
         // Load the main menu after the video finishes
-        SceneManager.LoadScene("Main Menu Final"); // Replace "MainMenu" with your actual main menu scene name
+        /*SceneManager.LoadScene("Main Menu Final");*/ // Replace "MainMenu" with your actual main menu scene name
+        LoadingScreenManager.Instance.StartLoading();
     }
 }
